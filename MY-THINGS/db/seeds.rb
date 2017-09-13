d20bf0e8b3e3ab82fae38ef0b52e658cae86b7b0
MARTINA_WEIDENBAUM <martinaweidenbaum1@MARTINA-WEIDENBAUMs-MacBook-Pro.local>
2017-09-13 13:39:05 -0400

    some changes

--- MY-THINGS/db/seeds.rb
+++ MY-THINGS/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+newSong = Thing.create(title: "Rock Your Body", genre: "pop", kind:"song", creator: "justin timberlake")
+"title"
+newPodcast = Thing.create(title: "Planet Money", genre: "economics", kind:"podcast", creator:"npr")
